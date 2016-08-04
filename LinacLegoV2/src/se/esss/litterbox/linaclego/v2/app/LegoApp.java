386c7ffdde2e206daca75d7490a967a575674341
david-mcginnis <dmcginnis427@gmail.com>
2016-08-04 06:33:20 +0200

    Updated Jar files and Java app date

--- LinacLegoV2/src/se/esss/litterbox/linaclego/v2/app/LegoApp.java
+++ LinacLegoV2/src/se/esss/litterbox/linaclego/v2/app/LegoApp.java
@@ -32,8 +32,8 @@ public class LegoApp extends JFrameSkeleton
 	private static final String frametitle = "LinacLego";
 	private static final String statusBarTitle = "Info";
 	private static final int numStatusLines = 10;
-	private static final String version = "v2.4";
-	private static final String versionDate = "July 16, 2016";
+	private static final String version = "v2.5";
+	private static final String versionDate = "August 4, 2016";
 
 	private Lego lego;
 	private JTabbedPane mainTabbedPane; 
